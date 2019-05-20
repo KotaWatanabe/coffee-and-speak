cc871c223049197555a72d67d00ffa0416496063
Kota Watanabe <kota3316@gmail.com>
2019-05-20 15:48:00 -0700

    create user and sessions controller

---
+++ src/server/app/controllers/user_controller.rb
@@ -0,0 +1,78 @@
+class UserController < ApplicationController
+    before_action :find_user, only: [:edit, :update, :change_password, :update_password]
+
+    def new 
+      @user = User.new
+    end
+
+    def create
+        @user = User.new user_params
+        if @user.save
+            session[:user_id] = @user.id
+            flash[:success] = "Registration successful!"
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+  
+    def update
+      if @user.update user_params
+        flash[:success] = "Your profile was updated successfully!"
+        redirect_to root_path
+      else
+        render :edit
+      end
+    end
+
+    def change_password
+    
+    end
+    
+    def update_password
+      if @user&.authenticate(params[:current_password])
+        if params[:new_password] == params[:current_password]
+          flash[:danger] = 'New password cannot be the same as old password!'
+          render :change_password
+        else  
+          check_and_update_password
+        end
+      else
+        flash[:danger] = "Your current password is invalid!"
+        render :change_password
+      end
+    end
+
+    
+    def dash_board
+      @favourited_teachers = current_user.favourited_teachers.order("favourites.created_at DESC")
+      @teacher = current_user.favourited_teachers
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :role, :password, :password_confirmation)
+    end
+    def find_user
+        @user = User.find(current_user.id)
+    end
+
+    def check_and_update_password
+        if params[:new_password] == params[:new_password_confirmation]
+          if @user.update(password: params[:new_password])
+            flash[:success] = "Password updated!"
+            redirect_to edit_user_path(current_user)
+          else  
+            flash[:danger] = @user.errors.full_messages.join(', ')
+            render :change_password
+          end
+        else
+          flash[:danger] = "Password confirmation does not match new password!"
+          render :change_password
+        end
+    end
+end
